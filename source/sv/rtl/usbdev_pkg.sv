8c7c2e6c11a7e0dee9768dcb1deb01520d92794f
amankv22 <126353155+amankv22@users.noreply.github.com>
2023-02-25 22:57:25 +0100

    Delete source/sv/rtl directory

--- source/sv/rtl/usbdev_pkg.sv
+++
@@ -1,7 +0,0 @@
-// Copyright lowRISC contributors.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-
-package usbdev_pkg;
-
-endpackage : usbdev_pkg
